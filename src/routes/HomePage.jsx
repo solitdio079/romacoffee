af1a4ecf628ccf13205c4698006956a7229d1798
solitdio079 <solitdio079@gmail.com>
2025-01-20 04:43:10 +0300

    here is the logo

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,10 +1,16 @@
-import {FaInstagram, FaFacebook} from 'react-icons/fa6'
+//import {FaInstagram, FaFacebook} from 'react-icons/fa6'
 import AnimatedLayout from '../../animation/AnimatedLayout'
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 export default function HomePage() {
     return (
       <AnimatedLayout>
+          <div
+          className=" flex-col min-h-screen"
+          style={{
+            backgroundImage: 'url(./wallpaper.png)',
+          }}
+        >
         <div className="navbar w-full bg-transparent">
           <div className="navbar-start">
             <div className="dropdown">
@@ -87,37 +93,8 @@ export default function HomePage() {
             </button>
           </div>
         </div>
-        <div
-          className="hero flex-col min-h-screen"
-          style={{
-            backgroundImage: 'url(./wallpaper.png)',
-          }}
-        >
-          <div className="hero-overlay bg-opacity-80"></div>
-          <div className="hero-content flex-col text-neutral-content text-center">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold text-white">
-                Yakında Kastamonu&apos;da!
-              </h1>
-              <p className="mb-5 flex flex-row justify-center text-white">
-                <a
-                  target="_blank"
-                  href={'https://www.instagram.com/romacoffeetea/'}
-                >
-                  <FaInstagram className="h-10 w-10 m-2" />
-                </a>
-                <a
-                  target="_blank"
-                  href="https://www.facebook.com/share/14VPJyGH6b/"
-                >
-                  <FaFacebook className="h-10 w-10 m-2" />
-                </a>
-              </p>
-              <Link to={'/about'} className="btn bg-[#aaa57b] text-white">
-                Hakkımızda
-              </Link>
-            </div>
-          </div>
+       
+         
         </div>
       </AnimatedLayout>
     )
