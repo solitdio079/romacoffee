3a1bc7cff60bfad29fdaa92cf4f896a01795f212
solitdio079 <solitdio079@gmail.com>
2025-01-24 04:06:31 +0300

    here we are

--- utils/images.js
+++ utils/images.js
@@ -1,35 +1,40 @@
 //import { Image } from 'react-grid-gallery'
 
-
+import gallery1 from '../public/gallery1.jpg'
+import gallery2 from '../public/gallery2.jpg'
+import gallery3 from '../public/gallery3.jpg'
+import gallery4 from '../public/gallery4.jpg'
+import gallery5 from '../public/gallery5.jpg'
+import gallery6 from '../public/gallery6.jpg'
 
 export const images = [
   {
-    src: './gallery1.jpg',
+    src: gallery1,
     width: 500,
     height: 800,
   },
   {
-    src: './gallery2.jpg',
+    src: gallery2,
     width: 400,
     height: 600,
   },
   {
-    src: './gallery3.jpg',
+    src: gallery3.jpg,
     width: 500,
     height: 800,
   },
   {
-    src: './gallery4.jpg',
+    src: gallery4,
     width: 400,
     height: 600,
   },
   {
-    src: './gallery5.jpg',
+    src: gallery5,
     width: 500,
     height: 800,
   },
   {
-    src: './gallery6.jpg',
+    src: gallery6,
     width: 400,
     height: 600,
   },
