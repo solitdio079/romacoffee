f3c1fce31bf6ea13194038c208158aef4be5174e
solitdio079 <solitdio079@gmail.com>
2025-01-24 04:47:13 +0300

    here we are 5

--- utils/images.jsx
+++ utils/images.jsx
@@ -3,37 +3,37 @@
 
 export const images = [
   {
-    src: './gallery1.jpg',
+   
     asset:'./gallery1.jpg',
     width: 500,
     height: 800,
   },
   {
-    src: './gallery2.jpg',
+    
     asset: './gallery2.jpg',
     width: 400,
     height: 600,
   },
   {
-    src: './gallery3.jpg',
+   
     asset: './gallery3.jpg',
     width: 500,
     height: 800,
   },
   {
-    src: './gallery4.jpg',
+    
     asset: './gallery4.jpg',
     width: 400,
     height: 600,
   },
   {
-    src: './gallery5.jpg',
+    
     asset: './gallery5.jpg',
     width: 500,
     height: 800,
   },
   {
-    src: './gallery6.jpg',
+    
     asset: './gallery6.jpg',
     width: 400,
     height: 600,
