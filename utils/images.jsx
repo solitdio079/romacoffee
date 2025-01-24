aad13578414f26059193975a2c864512a55adf9b
solitdio079 <solitdio079@gmail.com>
2025-01-24 04:10:22 +0300

    here we are

---
+++ utils/images.jsx
@@ -0,0 +1,35 @@
+//import { Image } from 'react-grid-gallery'
+
+
+export const images = [
+  {
+    src: './gallery1.jpg',
+    width: 500,
+    height: 800,
+  },
+  {
+    src: './gallery2.jpg',
+    width: 400,
+    height: 600,
+  },
+  {
+    src: './gallery3.jpg',
+    width: 500,
+    height: 800,
+  },
+  {
+    src: './gallery4.jpg',
+    width: 400,
+    height: 600,
+  },
+  {
+    src: './gallery5.jpg',
+    width: 500,
+    height: 800,
+  },
+  {
+    src: './gallery6.jpg',
+    width: 400,
+    height: 600,
+  },
+]
