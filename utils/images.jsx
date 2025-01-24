9933dca44171552b8e4a83cd913e2ec5686c7510
solitdio079 <solitdio079@gmail.com>
2025-01-24 05:16:19 +0300

    here we are masonry 2

--- utils/images.jsx
+++ utils/images.jsx
@@ -3,21 +3,27 @@
 
 export const images = [
   {
-    asset: 'https://www.romacoffeetea.com/gallery1.jpg',
+    asset: '/gallery1.jpg',
+    src: '/gallery1.jpg',
   },
   {
-    asset: './gallery2.jpg',
+    asset: '/gallery2.jpg',
+    src: '/gallery2.jpg',
   },
   {
-    asset: './gallery3.jpg',
+    asset: '/gallery3.jpg',
+    src: '/gallery3.jpg',
   },
   {
-    asset: './gallery4.jpg',
+    asset: '/gallery4.jpg',
+    src: '/gallery4.jpg',
   },
   {
-    asset: './gallery5.jpg',
+    asset: '/gallery5.jpg',
+    src: '/gallery5.jpg',
   },
   {
-    asset: './gallery6.jpg',
+    asset: '/gallery6.jpg',
+    src: '/gallery6.jpg',
   },
 ]
