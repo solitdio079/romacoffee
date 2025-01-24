346ae7430569c2eb7ae5e721c4aac8db738f386c
solitdio079 <solitdio079@gmail.com>
2025-01-24 03:57:18 +0300

    here we are

--- utils/images.js
+++ utils/images.js
@@ -5,21 +5,32 @@
 export const images = [
   {
     src: './gallery1.jpg',
+    width: 500,
+    height: 800,
   },
   {
     src: './gallery2.jpg',
+    width: 400,
+    height: 600,
   },
   {
     src: './gallery3.jpg',
+    width: 500,
+    height: 800,
   },
   {
     src: './gallery4.jpg',
+    width: 400,
+    height: 600,
   },
   {
     src: './gallery5.jpg',
+    width: 500,
+    height: 800,
   },
   {
     src: './gallery6.jpg',
+    width: 400,
+    height: 600,
   },
-  
 ]
